fffe6f428a007238c4fe4e2ab0e49c8dd5cc3b20
Lukas Werling <lukas.werling@gmail.com>
2016-01-17 18:15:41 +0100

    Add basic performance counter usage to the test program

--- read_pmu.c
+++ read_pmu.c
@@ -40,14 +40,32 @@ int main(int argc, char **argv)
 	// CPU printed above.
 	setaff(cpu);
 
-	uint32_t cycles;
-	MRC_PMU(cycles, PMCCNTR);
-	printf("Cycle Count: %"PRIu32"\n", cycles);
+	enable_pmn();
 
-	uint32_t pmceid0, pmceid1;
+	uint32_t pmcr, pmceid0, pmceid1;
+	MRC_PMU(pmcr, PMCR);
 	MRC_PMU(pmceid0, PMCEID0);
 	MRC_PMU(pmceid1, PMCEID1);
+	printf("PMCR: %"PRIx32"\n", pmcr);
 	printf("PMCEID0: %"PRIx32"\n", pmceid0);
 	printf("PMCEID1: %"PRIx32"\n", pmceid1);
+
+	set_pmn(0, CPU_CYCLES);
+	set_pmn(1, L1D_CACHE_LD);
+	set_pmn(2, L1D_CACHE_ST);
+	reset_pmn();
+	reset_ccnt();
+	// do some fake work
+	for (int i = 0; i < 50; i++) printf(".");
+	uint32_t cycles, cpu_cycles, l1d_cache_ld, l1d_cache_st;
+	MRC_PMU(cycles, PMCCNTR);
+	cpu_cycles = read_pmn(0);
+	l1d_cache_ld = read_pmn(1);
+	l1d_cache_st = read_pmn(2);
+	printf("\nCPU cycles: %"PRIu32"\n", cpu_cycles);
+	printf("Cycle Count: %"PRIu32"\n", cycles);
+	printf("Level 1 data cache access, read: %"PRIu32"\n", l1d_cache_ld);
+	printf("Level 1 data cache access, write: %"PRIu32"\n", l1d_cache_st);
+
 	return 0;
 }
